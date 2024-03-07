285c0c4afcb2b82717d512d1ac76b99d36d2400c
PM2600 <3262929659@qq.com>
2024-03-07 21:11:52 +0800

    Client

---
+++ IMChatClient/jsonStudy/protobufstudy/protobufstudy.cpp
@@ -0,0 +1,24 @@
+﻿// protobufstudy.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <fstream>
+#include "edy.pb.h"
+
+int main()
+{
+	EdyMsg msg;
+	msg.set_ncmd(0);
+	msg.set_strdata("hello world");
+	EmbeddedMessage* sub = new EmbeddedMessage();
+	sub->set_msubcmd(1);
+	sub->set_strparams("edy");
+	msg.set_allocated_submsg(sub);
+	std::string result = msg.SerializeAsString();	
+	msg.SerializePartialToOstream(&std::cout);
+	// 网络发送
+	EdyMsg msg2;
+	msg2.ParseFromString(result);
+	std::cout << "\ncmd: " << msg2.ncmd() << "\ndata: " << msg2.strdata() << std::endl;
+	return 0;
+}
